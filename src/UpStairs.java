9c2c7a76228892da15b6c3a7606eea62990d93cd
lzLrfo <rydhd11@gmail.com>
2022-02-08 00:52:55 +0900

    https://www.acmicpc.net/problem/2579

--- src/UpStairs.java
+++ src/UpStairs.java
@@ -10,7 +10,15 @@ public class UpStairs {
         for(int i = 0; i < n; i++)
             stairs[i] = Integer.parseInt(br.readLine());
         int[] dp = new int[n];
-        dp[0] = stairs[n-1];
-        
+        dp[0] = stairs[0];
+        if(n >= 2)
+            dp[1] = stairs[0] + stairs[1];
+        if(n >= 3)
+            dp[2] = Math.max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
+        if(n >= 4) {
+            for(int i = 3; i < n; i++)
+                dp[i] = Math.max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i];
+        }
+        System.out.println(dp[n-1]);
     }
 }
