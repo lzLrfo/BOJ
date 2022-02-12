8a37ec3330b054212a84c5cfa989731981995742
lzLrfo <rydhd11@gmail.com>
2022-02-12 18:03:49 +0900

    dynamic programming

---
+++ src/PinaryNumber.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PinaryNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n];
+        dp[0] = 1;
+        if(n >= 2)
+            dp[1] = 1;
+        if(n >= 3) {
+            for(int i = 2; i < n; i++) {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        System.out.println(dp[n-1]);
+    }
+}
