2f9207b625bc7a92d6b66f4c35e89195c1199612
lzLrfo <rydhd11@gmail.com>
2022-02-15 00:00:37 +0900

    hashmap

--- src/OrganizingFile.java
+++ src/OrganizingFile.java
@@ -1,12 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class OrganizingFile {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        HashMap<String, Integer> files = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), ".");
+            st.nextToken();
+            String extension = st.nextToken();
+            if(!files.containsKey(extension)) {
+                files.put(extension, 1);
+            }
+            else {
+                files.put(extension, files.get(extension) + 1);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        Object[] extensions = files.keySet().toArray();
+        Arrays.sort(extensions);
+        for(Object file : extensions) {
+            sb.append(file).append(" ").append(files.get(file)).append("\n");
+        }
+        System.out.println(sb);
     }
 }
